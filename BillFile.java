305773546f386f515b49cfb16e91d7c882d218c4
gaoym2023toj <gao.yiming.toj@gamil.com>
2024-07-26 09:24:22 +0900

    first commit

---
+++ BillFile.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class BillFile {
+
+	private int fileno;
+
+	private String billno;
+	
+	private String billFilenm;
+
+	private String billFilelink;
+	
+	private String billFilehash;
+
+}
